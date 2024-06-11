8dc16eea09fbfbff44fda19ae4c19948da95abd8
SheepTAO <sheeptao@outlook.com>
2024-06-12 00:42:05 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11'
     ],
 )
